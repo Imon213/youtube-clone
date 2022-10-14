f291825599e10b0d3f2ea2616a6423ddc7bd05e1
unknown <emonf666@gmail.com>
2022-10-14 23:39:11 +0600

    component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
+import "./App.css";
+
+import { Box } from "@mui/material";
+import ChannelDetail from "./Component/ChannelDetail/ChannelDetail";
+import Navbar from "./Component/Navbar/Navbar";
+import VideoDetail from "./Component/VideoDetail/VideoDetail";
+import SearchFeed from "./Component/SearchFeed/SearchFeed";
+import Feed from "./Component/Feed/Feed";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Box sx={{ background: "#000" }} />
+        <Navbar />
+       
+          <Routes>
+          <Route exact path="/" element={<Feed />} />
+          <Route exact path="/channel/:id" element={<ChannelDetail />} />
+          <Route exact path="/video/:id" element={<VideoDetail />} />
+          <Route exact path="/search/:searchTerm" element={<SearchFeed />} />
+          </Routes>
+       
+      </Router>
     </div>
   );
 }
